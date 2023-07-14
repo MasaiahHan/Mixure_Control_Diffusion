09efc6ca642f981d1e1cc7309c5b9e464d938e20
MasaiahHan <hantianyang522@gmail.com>
2023-07-14 14:59:39 +0800

    commit

---
+++ mixdiff/__init__.py
@@ -0,0 +1,5 @@
+from .canvas import Image2ImageRegion, RerollRegion, StableDiffusionCanvasPipeline, Text2ImageRegion
+from .imgtools import preprocess_image
+from .tiling import StableDiffusionTilingPipeline
+from .canvas_multicontrolnet import StableDiffusionCanvasControlnetPipeline
+# from .canvas_multicontrolnet import StableDiffusionCanvasControlnetPipeline
